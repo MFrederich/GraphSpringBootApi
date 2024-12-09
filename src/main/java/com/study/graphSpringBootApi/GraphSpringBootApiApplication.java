5d52963e7ff3bd1f2ca9b6b03b10bffa6e5eab94
frederich <frederich.alex@gmail.com>
2024-12-08 21:16:28 -0300

    commit inicial

---
+++ src/main/java/com/study/graphSpringBootApi/GraphSpringBootApiApplication.java
@@ -0,0 +1,13 @@
+package com.study.graphSpringBootApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphSpringBootApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphSpringBootApiApplication.class, args);
+    }
+
+}
